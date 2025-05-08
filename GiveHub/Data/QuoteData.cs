7d26c41e5fe7742ea2780662749f1380c6dba8ba
MJSuttles <rolltiderolldad@gmail.com>
2025-05-07 19:02:46 -0500

    added all folders for Quotes entity

---
+++ GiveHub/Data/QuoteData.cs
@@ -0,0 +1,13 @@
+using GiveHub.Models;
+
+namespace GiveHub.Data
+{
+  public class QuoteData
+  {
+    public static List<Quote> Quotes = new()
+    {
+      // seed data
+
+    };
+  }
+}
---
+++ GiveHub/Endpoints/QuoteEndpoints.cs
@@ -0,0 +1,52 @@
+using GiveHub.Interfaces;
+using GiveHub.Models;
+
+namespace GiveHub.Endpoint
+{
+  public static class QuoteEndpoints
+  {
+    // The endpoint layer is responsible for handling HTTP requests.
+    // The endpoint layer will call the service layer to process business logic.
+    // The endpoint layer will return the data to the client.
+    // The endpoint layer is the entry point for the client to access the application.
+    // We must register this MapWeatherEndpoints method in the Program.cs file.
+    // You can click the reference to see where it is registered in the Program.cs file.
+
+    public static void MapTagEndpoints(this IEndpointRouteBuilder routes)
+    {
+      var group = routes.MapGroup("/api/quotes").WithTags(nameof(Tag));
+
+      // API calls
+
+      // example
+
+      // group.MapGet("/", async (ISimplyBooksAuthorService simplyBooksAuthorService) =>
+      // {
+      //   return await simplyBooksAuthorService.GetAllAuthorsAsync();
+      // })
+      // .WithName("GetAllAuthors")
+      // .WithOpenApi()
+      // .Produces<List<Author>>(StatusCodes.Status200OK);
+
+      // Status StatusCodes
+
+      // GET Calls:
+
+      // .Produces<List<Author>>(StatusCodes.Status200OK);
+
+      // POST Calls:
+
+      // .Produces<Author>(StatusCodes.Status201Created)
+      // .Produces(StatusCodes.Status400BadRequest);
+
+      // PUT Calls:
+
+      // .Produces<Author>(StatusCodes.Status201Created)
+      // .Produces(StatusCodes.Status400BadRequest);
+
+      // DELETE Calls:
+
+      // .Produces<Author>(StatusCodes.Status204NoContent);
+    }
+  }
+}
